04772c5d5d4a92887f83594c24e3308681d9d637
washinofumihiro1991 <f_washino@yahoo.co.jp>
2016-10-27 07:03:37 +0900

    update test

--- daily_report/day/test_report.py
+++ daily_report/day/test_report.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 from django.test.client import Client
 from django.db.models import Q
 from django.test import TestCase, RequestFactory
-
+from django.http import HttpRequest
+from .views import report_edit
 
 # 日報作成や削除、編集のテスト
 class ReportTest(TestCase):
@@ -22,16 +23,28 @@ class ReportTest(TestCase):
         cls.password = 'tanakashipassword'
         cls.no_password = ""
         cls.error_password = "error_password"
-        client = Client()
-        client.post('/register',
-                    {'user_id': cls.user_id,
-                     'password': cls.password,
-                     'mail_address': cls.mail_address})
-        Client().login(username=cls.user_id, password=cls.password)
+        # client = Client()
+        # client.post('/register',
+        #             {'user_id': cls.user_id,
+        #              'password': cls.password,
+        #              'mail_address': cls.mail_address})
+        # Client().login(username=cls.user_id, password=cls.password)
 
     @classmethod
     def tearDownClass(cls):
         print("Success test")
+
+    def post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        # request.user.username = self.user
+        request.POST['user'] = self.user
+        request.POST['title'] = self.title
+        request.POST['content_Y'] = self.content_Y
+        request.POST['content_W'] = self.content_W
+        request.POST['content_T'] = self.content_T
+        # print(request.user.is_authenticated())
+        return request
 
     # データベースの初期状態の確認(何も入力されていないか確認)
     def test_init_database(self):
@@ -41,28 +54,36 @@ class ReportTest(TestCase):
     # 日報の各項目の入力、編集、削除をきるか確認
     def test_report_add(self):
         # 各データの入力と呼び出し
+        # client = Client()
+        # client.post('/report/add/',
+        #             {'title': self.title,
+        #              'user': self.user,
+        #              'content_Y': self.content_Y,
+        #              'content_W': self.content_W,
+        #              'content_T': self.content_T,
+        #              'report_id': None,
+        #              'request.user.username': self.user,
+        #              })
         client = Client()
-        client.post('/report/add/',
-                    {'title': self.title,
-                     'user': self.user,
-                     'content_Y': self.content_Y,
-                     'content_W': self.content_W,
-                     'content_T': self.content_T,
-                     'report_id': None,
-                     'request.user.username': self.user,
-                     })
-
+        client.post('/register',
+                    {'user_id': self.user_id,
+                     'password': self.password,
+                     'mail_address': self.mail_address})
+        Client().login(username=self.user_id, password=self.password)
         print(Report.objects.all().count())
 
-        report = Report.objects.get(user=self.user)
+        # report = Report.objects.get(user=self.user)
+        report = report_edit(self.post_request(), None)
+        print(Report.objects.all().count())
+        # print(report)
 
         # report = create_report(self, self.title[0], self.content[0], self.user[0], self.time[0])
         #
         # # 各データが一致しているかを確認
-        self.assertEquals(report.title, self.title[0])
-        self.assertEquals(report.content_Y, self.content[0])
-        self.assertEquals(report.user, self.user[0])
-        self.assertEquals(report.user_post_time, self.time[0])
+        # self.assertEquals(report.title, self.title[0])
+        # self.assertEquals(report.content_Y, self.content[0])
+        # self.assertEquals(report.user, self.user[0])
+        # self.assertEquals(report.user_post_time, self.time[0])
         #
         # # 日報の編集
         # report.title = self.title[1]
